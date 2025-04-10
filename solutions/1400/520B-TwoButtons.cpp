b532d062abdcffe0de274a12291d4482de4cbdc2
0xDarwinC <darwinchris.edu0525@gmail.com>
2025-04-09 22:17:41 -0400

    finished 520B, learned that g++ does bitshifting for me

---
+++ solutions/1400/520B-TwoButtons.cpp
@@ -0,0 +1,80 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cerr<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+const int MX_SZ=1e5+5;
+int par[MX_SZ];
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+
+// Recursively finds next jumping point
+int helper(int n, int m){
+    if(m<=n){
+        return n-m;
+    }
+    else{
+        if(m%2 == 0){
+            if(n >= m/2 && n != 1){
+                return min(n - (m>>1) + 1, (n<<1) - m + 1);
+            }
+            else{
+                int jump = n;
+                int tally = 0;
+                while(jump < m){
+                    jump = jump << 1;
+                    tally+=1;
+                }
+                return min(jump - m + tally, helper(n, (m>>1))+1);
+            }
+        }
+        else{
+            if(n >= ((m+1)>>1) && n != 1){
+                return min(n - ((m+1)>>1) + 2, (n<<1) - m + 1);
+            }
+            else{
+                int jump = n;
+                int tally = 0;
+                while(jump < m){
+                    jump<<= 1;
+                    tally+=1;
+                }
+                return min(jump - m + tally, helper(n, ((m+1)>>1))+2);
+            }
+        }
+    }
+}
+
+int32_t main(){
+    int n, m;
+    cin>>n>>m;
+    cout<<helper(n,m);
+}
