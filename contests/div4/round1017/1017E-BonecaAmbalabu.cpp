d374e1839e4c0255dd3f2f6d5c55dbc01711ce48
0xDarwinC <darwinchris.edu0525@gmail.com>
2025-04-15 14:16:57 -0400

    Started 1017E from contest, updated code snippets

--- contests/div4/round1017/1017E-BonecaAmbalabu.cpp
+++ contests/div4/round1017/1017E-BonecaAmbalabu.cpp
@@ -5,10 +5,15 @@
 #define vvi vector<vi >
 #define vb vector<bool>
 #define vvb vector<vb >
+#define pi pair<int,int >
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
 #define fr(i,n) for(int i=0; i<(n); i++)
 #define rep(i,a,n) for(int i=(a); i<=(n); i++)
 #define nl cout<<"\n"
-#define dbg(var) cerr<<#var<<"="<<var<<" "
+#define dbg(var) cout<<#var<<"="<<var<<" "
 #define all(v) v.begin(),v.end()
 #define srt(v)  sort(v.begin(),v.end())         // sort 
 #define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
@@ -34,7 +39,46 @@ template<typename typC,typename typD> ostream &operator<<(ostream &cout,const ve
 template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
 // ===================================END Of the input module ==========================================
 
-
-int32_t main(){
-
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    vi nums;
+    int t;
+    cin>>t;
+    int len;
+    int x;
+    int max;
+    int bestsum;
+    int sum;
+    int bestnum;
+    unordered_map<int,int> memo;
+    cout<<(mp(1,2))<<"hellyea";
+    fr(i,t){
+        cin>>len;
+        nums=vi();
+        max = -1;
+        bestsum=-1;
+        bestnum = -1;
+        fr(j,len){
+            cin>>x;
+            if(x>max){
+                max=x;
+            }
+            cout<<bin(x,8)<<" ";
+            nums.pb(x);
+        }
+        cout<<"\n";
+        fr(j,len){
+            sum = 0;
+            fr(k,len){
+                sum += (nums[k]^nums[j]);
+            }
+            if(sum>bestsum){
+                bestsum=sum;
+                bestnum=nums[j];
+            }
+        }
+        cout<<bestsum<<"and best"<<bestnum<<'\n';
+    }
+    return 0;
 }
