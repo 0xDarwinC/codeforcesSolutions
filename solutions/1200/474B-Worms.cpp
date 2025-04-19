f16a0cf0c7a6e73eebae7435e09bf446f2c154df
0xDarwinC <darwinchris.edu0525@gmail.com>
2025-04-19 17:40:45 -0400

    quick 1200 prefixsum+binsearch problem

---
+++ solutions/1200/474B-Worms.cpp
@@ -0,0 +1,78 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define pi pair<int,int >
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+const int MX_SZ=1e5+5;
+int par[MX_SZ];
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    //strategy -- prefix sum to show which pile the juicy ones exist in. Use bin search for finding the bin of the juicy.
+    int curr = 0;
+    int n, m, x; cin>>n;
+    vi piles;
+    fr(i,n){
+        cin>>x;
+        curr+=x;
+        piles.pb(curr);
+    }
+    cin>>m;
+    int juicy;
+    int l,r,mid,best;
+    fr(i,m){
+        l=0;
+        r=n-1;
+        best=n+1;
+        cin>>juicy;
+        while(l<=r){
+            mid = (l+r)/2;
+            if(juicy>piles[mid]){
+                l=mid+1;
+            }
+            else{
+                if(mid<best){
+                    best=mid;
+                }
+                r=mid-1;
+            }
+        }
+        
+        cout<<(best+1)<<'\n';
+    }
+    return 0;
+}
