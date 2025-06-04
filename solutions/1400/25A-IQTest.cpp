c84fd79cffb14a4ceb6bb8d6542ac9c77942c1fa
0xDarwinC <157836586+0xDarwinC@users.noreply.github.com>
2025-06-04 10:14:27 -0400

    quick 1300 problem, sliding window and parity

---
+++ solutions/1400/25A-IQTest.cpp
@@ -0,0 +1,75 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define pi pair<int,int >
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl "\n"
+#define dbg(var) cerr<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+ 
+const int MX_SZ=1e5+5;
+int par[MX_SZ];
+ 
+ 
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+ 
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int parity;
+    int n, x;
+    int i = 0;
+    cin>>n;
+    cin>>x;
+    parity = (x%2);
+    cin>>x;
+    i++;
+    if(x%2 != parity){
+        cin>>x;
+        if(x%2 != parity){
+            cout<<1;
+            return 0;
+        }
+        else{
+            cout<<2;
+            return 0;
+        }
+    }
+    while(i<=n-2){
+        cin>>x;
+        i++;
+        if(x%2 != parity){
+            cout<<(i+1);
+            return 0;
+        }
+    }
+    
+    return 0;
+}
