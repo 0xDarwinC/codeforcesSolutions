bf3e3cde74d90821c5caf96c559fe1f032abc337
0xDarwinC <darwinchris.edu0525@gmail.com>
2025-11-01 14:04:23 -0400

    2167 contest solutions (G is wrong), and also a 1400 problem from today

---
+++ contests/div4/round2167/2167G-MukhammadaliAndTheSmoothArray.cpp
@@ -0,0 +1,102 @@
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define pi pair<int,int >
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl "\n"
+#define dbg(var) cerr<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+const int MX_SZ=1e5+5;
+int par[MX_SZ];
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t; cin>>t;
+    int n;
+    int x;
+    int cost=0;
+    int j = 0;
+    vi nums;
+    vi a;
+    fr(i,t){
+        cin>>n;
+        if(n==1){
+            cin>>x;
+            cin>>x;
+            cout<<0<<endl;
+        }
+        else{
+            fr(i,n){
+                cin>>x;
+                nums.pb(x);
+            }
+            fr(i,n){
+                cin>>x;
+                a.pb(x);
+            }
+            cost=0;
+            j=0;
+            while(j<n-1){
+                if(nums[j]>nums[j+1]){
+                    if(a[j]<=a[j+1]){
+                        if(j>0){
+                            if(nums[j-1]>nums[j+1]){
+                                nums[j+1]=nums[j];
+                                cost+=a[j+1];
+                            }
+                            else{
+                                nums[j]=nums[j-1];
+                                cost+=a[j];
+                            }
+                        }
+                        else{
+                            nums[j]=INT_MIN;
+                            cost+=a[j];
+                        }
+                    }
+                    else{
+                        nums[j+1]=nums[j];
+                        cost+=a[j+1];
+                    }
+                }
+                j+=1;
+            }
+            cout<<cost<<endl;
+            nums.clear();
+            a.clear();
+        }
+    }
+
+    return 0;
+}
